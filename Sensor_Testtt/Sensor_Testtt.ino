890f72d7966700bf8ef120da5fc8d7537ad1ac7a
QuizziatorLoL <dagreatquizinnie@gmail.com>
2025-10-30 20:35:30 -0400

    Create Sensor_Testtt.ino
    Sensor tester code

---
+++ Sensor_Testtt/Sensor_Testtt.ino
@@ -0,0 +1,14 @@
+#include <Adafruit_CircuitPlayground.h>
+uint16_t value;
+uint16_t freq;
+///////////////////////////////////////////////////////////////////////////////
+void setup() {
+Serial.begin(9600);
+CircuitPlayground.begin();
+}
+///////////////////////////////////////////////////////////////////////////////
+void loop() {
+value = analogRead(A3);
+freq = map(value, 0, 1023, 100, 10000);
+//CircuitPlayground.playTone(freq, 100, false);
+}
